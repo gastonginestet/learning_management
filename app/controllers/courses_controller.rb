81b609fc82cd43b012fdb6035cee7648eba0a76f
Gaston Ginestet <gastonauginestet@gmail.com>
2024-03-26 11:22:13 -0300

    user tracking finished

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,14 @@ class CoursesController < ApplicationController
   # GET /courses or /courses.json
   def index
     @courses = Course.all
+    @user_started_courses = current_user&.lesson_users&.joins(:lesson)&.pluck(:course_id)&.uniq
+    if @user_started_courses.present?
+      @user_course_progresses = @user_started_courses.map do |course_id|
+        course_lessons = Course.find(course_id).lessons.count
+        completed_lessons = current_user&.lesson_users&.joins(:lesson)&.where(completed: true, lesson: { course: course_id })&.count
+        { course_id: course_id, completed_percentage: (completed_lessons.to_f / course_lessons.to_f * 100).to_i }
+      end
+    end
   end
 
   # GET /courses/1 or /courses/1.json
@@ -49,23 +57,16 @@ class CoursesController < ApplicationController
   end
 
   # DELETE /courses/1 or /courses/1.json
-  def destroy
-    @course.destroy
-
-    respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(:title, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
 end
